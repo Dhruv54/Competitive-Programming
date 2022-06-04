26e5cf7d49f29d54d0f592035b692b8f4ba72784
Dhruv <190310patel@gmail.com>
2022-06-04 14:49:28 +0530

    buysellstock,houserobber,mergesortedarray

---
+++ 88_MergeSortedArray_lc.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if(m==0)
+        {
+            nums1[m]=nums2[n-1];
+        }
+        int last=m+n-1;
+        m=m-1;
+        n=n-1;
+        while(m>=0 && n>=0)
+        {
+            if(nums1[m]>nums2[n])
+            {
+                nums1[last]=nums1[m];
+                m-=1;
+            }else{
+                nums1[last]=nums2[n];
+                n-=1;
+            }
+            last-=1;
+        }
+        
+        while(n>=0){
+            nums1[last]=nums2[n];
+            last-=1;
+            n-=1;
+        }
+    }
+};
